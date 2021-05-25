fbbad14c9c9b71a42d80028daa3bdeaee645791f
Carl Johnson <me@carlmjohnson.net>
2021-05-25 14:54:53 -0400

    TestRecord: use temp directory; really make the request

--- recorder_test.go
+++ recorder_test.go
@@ -11,22 +11,23 @@ import (
 
 func TestRecord(t *testing.T) {
 	cl := *http.DefaultClient
-	cl.Transport = requests.Record(cl.Transport, "testdata")
+	dir := t.TempDir()
+	cl.Transport = requests.Record(http.DefaultTransport, dir)
 	var s1, s2 string
 	err := requests.URL("http://example.com").
 		Client(&cl).
 		ToString(&s1).
 		Fetch(context.Background())
 	if err != nil {
-		log.Fatal("unexpected error", err)
+		log.Fatalln("unexpected error:", err)
 	}
-	cl.Transport = requests.Replay("testdata")
+	cl.Transport = requests.Replay(dir)
 	err = requests.URL("http://example.com").
 		Client(&cl).
 		ToString(&s2).
 		Fetch(context.Background())
 	if err != nil {
-		log.Fatal("unexpected error", err)
+		log.Fatalln("unexpected error:", err)
 	}
 	if s1 != s2 {
 		log.Fatalf("%q != %q", s1, s2)
