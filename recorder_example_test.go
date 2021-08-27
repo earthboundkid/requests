54df5171b4374bbf0bacee6d82e4ea8a4cbdda72
Carl Johnson <me@carlmjohnson.net>
2021-08-27 15:40:39 -0400

    ReplayFS: Allow arbitrary file prefixes as comments

--- recorder_example_test.go
+++ recorder_example_test.go
@@ -11,7 +11,7 @@ import (
 
 func ExampleReplayFS() {
 	fsys := fstest.MapFS{
-		"MKIYDwjs.res.txt": &fstest.MapFile{
+		"fsys.example - MKIYDwjs.res.txt": &fstest.MapFile{
 			Data: []byte(`HTTP/1.1 200 OK
 Content-Type: text/plain; charset=UTF-8
 Date: Mon, 24 May 2021 18:48:50 GMT
