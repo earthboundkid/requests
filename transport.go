ba6b93e7f79a008b08cc1ee7e79cf8fc6a1daded
Carl Johnson <me@carlmjohnson.net>
2021-09-08 13:30:19 -0400

    Remove WrapTransport, WrapRoundTripper
    It's a little too high level and too low level at once. Easier to just
    tell people to use RoundTripFuncs.

--- transport.go
+++ transport.go
@@ -6,24 +6,6 @@ import (
 	"strings"
 )
 
-// WrapTransport sets c.Transport to a WrapRoundTripper.
-func WrapTransport(c *http.Client, f func(r *http.Request)) {
-	c.Transport = WrapRoundTripper(c.Transport, f)
-}
-
-// WrapRoundTripper deep clones a request and passes it to f before calling the underlying
-// RoundTripper. If rt is nil, it calls http.DefaultTransport.
-func WrapRoundTripper(rt http.RoundTripper, f func(r *http.Request)) http.RoundTripper {
-	if rt == nil {
-		rt = http.DefaultTransport
-	}
-	return RoundTripFunc(func(r *http.Request) (*http.Response, error) {
-		r = r.Clone(r.Context())
-		f(r)
-		return rt.RoundTrip(r)
-	})
-}
-
 // RoundTripFunc is an adaptor to use a function as an http.RoundTripper.
 type RoundTripFunc func(req *http.Request) (res *http.Response, err error)
 
