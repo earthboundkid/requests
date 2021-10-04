f8e8fcccf536c4d685a2c99d91e7a0f3946d90c8
Carl Johnson <me@carlmjohnson.net>
2021-10-04 12:35:27 -0400

    Replace AddCookieJar with NewCookieJar

--- cookies.go
+++ cookies.go
@@ -7,13 +7,12 @@ import (
 	"golang.org/x/net/publicsuffix"
 )
 
-// AddCookieJar adds the standard public suffix list cookiejar to an http.Client.
-// Because it modifies the client in place, cl should not be nil.
-func AddCookieJar(cl *http.Client) {
+// NewCookieJar returns a cookie jar using the standard public suffix list.
+func NewCookieJar() http.CookieJar {
 	jar, err := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	// As of Go 1.16, cookiejar.New err is hardcoded nil
 	if err != nil {
 		panic(err)
 	}
-	cl.Jar = jar
+	return jar
 }
