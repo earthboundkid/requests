ebef71e47c409eeb22242076346806238844f98d
Carl Johnson <me@carlmjohnson.net>
2021-10-20 21:46:35 -0400

    MaxFollow: More clear docs

--- redirects_example_test.go
+++ redirects_example_test.go
@@ -8,44 +8,48 @@ import (
 	"github.com/carlmjohnson/requests"
 )
 
-func ExampleNoFollow() {
+func ExampleCheckRedirectPolicy() {
 	cl := *http.DefaultClient
 	cl.CheckRedirect = requests.NoFollow
 
-	var h http.Header
 	if err := requests.
 		URL("https://httpbingo.org/redirect/1").
 		Client(&cl).
 		CheckStatus(http.StatusFound).
 		Handle(func(res *http.Response) error {
-			h = res.Header
+			fmt.Println("Status", res.Status)
+			fmt.Println("From", res.Request.URL)
+			fmt.Println("To", res.Header.Get("Location"))
 			return nil
 		}).
 		Fetch(context.Background()); err != nil {
 		panic(err)
 	}
-	fmt.Println(h.Get("Location"))
 	// Output:
-	// /get
+	// Status 302 Found
+	// From https://httpbingo.org/redirect/1
+	// To /get
 }
 
 func ExampleMaxFollow() {
 	cl := *http.DefaultClient
 	cl.CheckRedirect = requests.MaxFollow(1)
 
-	var h http.Header
 	if err := requests.
 		URL("https://httpbingo.org/redirect/2").
 		Client(&cl).
 		CheckStatus(http.StatusFound).
 		Handle(func(res *http.Response) error {
-			h = res.Header
+			fmt.Println("Status", res.Status)
+			fmt.Println("From", res.Request.URL)
+			fmt.Println("To", res.Header.Get("Location"))
 			return nil
 		}).
 		Fetch(context.Background()); err != nil {
 		panic(err)
 	}
-	fmt.Println(h.Get("Location"))
 	// Output:
-	// /get
+	// Status 302 Found
+	// From https://httpbingo.org/relative-redirect/1
+	// To /get
 }
