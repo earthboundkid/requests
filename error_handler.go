425158b593f39150a68ac577c0fd9d4ff89273f6
Carl Johnson <me@carlmjohnson.net>
2022-10-31 10:00:49 -0400

    Change ErrorHandler signature

--- error_handler.go
+++ error_handler.go
@@ -1,18 +1,27 @@
 package requests
 
 import (
+	"errors"
 	"net/http"
 )
 
 // ErrorHandler is a function accepted by Builder.OnError.
-type ErrorHandler = func(error, *http.Request, *http.Response)
+// Note that the error, request, and response may all be nil
+// depending on the error encountered and the effect of prior handlers.
+type ErrorHandler = func(ErrorKind, error, *http.Request, *http.Response) error
 
 // ValidatorHandler converts a ResponseHandler into an ErrorHandler for invalid responses.
 // The ResponseHandler only runs if the ErrorHandler encounters a validation error.
+// If the ResponseHandler succeeds, ErrInvalidHandled is returned.
 func ValidatorHandler(h ResponseHandler) ErrorHandler {
-	return func(err error, req *http.Request, res *http.Response) {
-		if res != nil && HasKindErr(err) == KindInvalidErr {
-			h(res)
+	return func(kind ErrorKind, err error, req *http.Request, res *http.Response) error {
+		if kind == KindInvalidErr && res != nil {
+			if err := h(res); err != nil {
+				return err
+			}
 		}
+		return ErrInvalidHandled
 	}
 }
+
+var ErrInvalidHandled = errors.New("handled recovery from invalid response")
