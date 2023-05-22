c343f00f5ebb610d8f4644ebdc75b6440602c558
Carl Johnson <me@carlmjohnson.net>
2023-05-22 09:47:17 -0400

    ExampleError: More consistent struct

--- reqxml/err_example_test.go
+++ reqxml/err_example_test.go
@@ -13,10 +13,10 @@ import (
 func ExampleError() {
 	type ErrorXML struct {
 		XMLName   xml.Name `xml:"Error"`
-		Code      string
-		Message   string
-		RequestID string `xml:"RequestId"`
-		HostID    string `xml:"HostId"`
+		Code      string   `xml:"Code"`
+		Message   string   `xml:"Message"`
+		RequestID string   `xml:"RequestId"`
+		HostID    string   `xml:"HostId"`
 	}
 
 	var errObj ErrorXML
