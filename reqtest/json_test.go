4278463774afd078c7268f3a43a2343742f42491
Carlana <mail@carlana.net>
2025-09-10 11:08:21 -0400

    TestReplayJSON: Better tests

--- reqtest/json_test.go
+++ reqtest/json_test.go
@@ -10,9 +10,28 @@ import (
 )
 
 func TestReplayJSON(t *testing.T) {
+	// Marshals JSON
+	var s string
 	err := requests.
+		URL("http://example.com/api").
+		Transport(reqtest.ReplayJSON(200, "Hello")).
+		ToString(&s).
+		Fetch(context.Background())
+	be.NilErr(t, err)
+	be.Equal(t, `"Hello"`, s)
+
+	// Returns marshal errors
+	err = requests.
 		URL("http://example.com/api").
 		Transport(reqtest.ReplayJSON(200, make(chan int))).
 		Fetch(context.Background())
 	be.Nonzero(t, err)
+
+	// Sets status code
+	err = requests.
+		URL("http://example.com/api").
+		Transport(reqtest.ReplayJSON(500, "Error")).
+		Fetch(context.Background())
+	be.Nonzero(t, err)
+	be.True(t, requests.HasStatusErr(err, 500))
 }
